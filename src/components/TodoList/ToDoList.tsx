a8dcb7d3d3e33c93097ca88acc7df85ea1144c9d
bernardo Souza <bernardombsouza@gmail.com>
2023-12-15 16:10:50 -0300

    chore: dynamic render list of list length

--- src/components/TodoList/ToDoList.tsx
+++ src/components/TodoList/ToDoList.tsx
@@ -1,7 +1,25 @@
 import styles from './ToDoList.module.css'
-// import clipboard from '../../assets/Clipboard.png'
-const list = [1, 2, 3, 4, 5]
+import clipboard from '../../assets/Clipboard.png'
+const list: string[] = ['Deletar', 'Deletar', 'Deletar']
+
 export function ToDoList() {
+
+  if (list.length == 0) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.headerToDoList}>
+          <p>Tarefas criadas</p>
+          <p>Concluidas</p>
+        </div>
+        <div className={styles.toDoList}>
+          <img src={clipboard} />
+          <strong>Você ainda não tem taredas cadastradas</strong>
+          <span>Crie Tarefas e organize seus itens a fazer</span>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.headerToDoList}>
@@ -10,19 +28,14 @@ export function ToDoList() {
       </div>
       <div className={styles.toDoList}>
         {
-          list.map(() => {
-            return (
-              <div className={styles.content}>
-                <div className={styles.check}></div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, in doloremque pariatur vitae vero hic, molestias fuga similique, minus earum optio molestiae aut consequuntur. Voluptates alias iste consectetur fugiat odit!</p>
-                <p>Lixeira</p>
-              </div>
-            )
-          })
+          list.map(item => (
+            <div className={styles.content}>
+              <div className={styles.check}></div>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, in doloremque pariatur vitae vero hic, molestias fuga similique, minus earum optio molestiae aut consequuntur. Voluptates alias iste consectetur fugiat odit!</p>
+              <p>{item}</p>
+            </div>
+          ))
         }
-        {/* <img src={clipboard} />
-        <strong>Você ainda não tem taredas cadastradas</strong>
-        <span>Crie Tarefas e organize seus itens a fazer</span> */}
       </div>
     </div>
   )
