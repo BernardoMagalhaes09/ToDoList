dfb32c972ea3bd647a771c31fe402eaa78b12937
bernardo Souza <bernardombsouza@gmail.com>
2023-12-15 15:49:05 -0300

    chore: add change table todos in code

--- src/components/TodoList/ToDoList.tsx
+++ src/components/TodoList/ToDoList.tsx
@@ -1,6 +1,6 @@
 import styles from './ToDoList.module.css'
-import clipboard from '../../assets/Clipboard.png'
-
+// import clipboard from '../../assets/Clipboard.png'
+const list = [1, 2, 3, 4, 5]
 export function ToDoList() {
   return (
     <div className={styles.container}>
@@ -9,9 +9,20 @@ export function ToDoList() {
         <p>Concluidas</p>
       </div>
       <div className={styles.toDoList}>
-        <img src={clipboard} />
+        {
+          list.map(() => {
+            return (
+              <div className={styles.content}>
+                <div className={styles.check}></div>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, in doloremque pariatur vitae vero hic, molestias fuga similique, minus earum optio molestiae aut consequuntur. Voluptates alias iste consectetur fugiat odit!</p>
+                <p>Lixeira</p>
+              </div>
+            )
+          })
+        }
+        {/* <img src={clipboard} />
         <strong>Você ainda não tem taredas cadastradas</strong>
-        <span>Crie Tarefas e organize seus itens a fazer</span>
+        <span>Crie Tarefas e organize seus itens a fazer</span> */}
       </div>
     </div>
   )
